458fff866ce5e7ce3abd86f45ffbea118f427181
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-02-01 00:19:28 +0100

    input field

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.button import MDRectangleFlatButton, MDTextButton
 
 
@@ -15,5 +16,5 @@ class Countify(MDApp):
     def build(self):
         return Container()
 
-Window.size = (400, 800)
+Window.size = (300, 500)
 Countify().run()
