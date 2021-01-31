34a130bc734d19de950aedf93515f291202c6b51
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-01-31 22:05:50 +0100

    basic app look

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from kivy.uix.screenmanager import Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import builder
+from kivy.core.window import Window
 
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.button import MDRectangleFlatButton, MDTextButton
 
 
-class Container(FloatLayout):
+
+class Container(MDBoxLayout):
     pass
 
 class Countify(MDApp):
     def build(self):
-        # screen = Screen()
-        # screen.add_widget(
-        #     MDRectangleFlatButton(
-        #         text="Ustal budżet",
-        #         pos_hint={"center_x": 0.5, "center_y": 0.55},
-        #     )
-        # )
-        # screen.add_widget(    
-        #     MDRectangleFlatButton(
-        #         text="Historia zakupów",
-        #         pos_hint={"center_x": 0.5, "center_y": 0.45},
-        #     )
-        # )
-
         return Container()
 
+Window.size = (400, 800)
 Countify().run()
