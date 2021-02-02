052b152822f14e37e4b30c3e604eb231175d3ec2
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-02-02 23:05:22 +0100

    my nurse basic setup

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from kivy.lang import builder
 from kivy.core.window import Window
 
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.button import MDRectangleFlatButton, MDTextButton
-
-
 
 class Container(MDBoxLayout):
     pass
