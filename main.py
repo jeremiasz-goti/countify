9dcf7f7951a3cdd508a2f57d22d001ef00f5d709
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-02-04 22:24:35 +0100

    basic screen manager

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 
-class Container(MDBoxLayout):
+class HomeScreen(Screen):
     pass
+
+class PillScreen(Screen):
+    pass
+
+class DoctorScreen(Screen):
+    pass
+
+class BookScreen(Screen):
+    pass
+
+class ViewManager(ScreenManager):
+    pass
+
+
 
 class mynurse(MDApp):
     def build(self):
-        return Container()
+        kv = Builder.load_file('main.kv')
+        return kv
 
 Window.size = (300, 500)
 mynurse().run()
